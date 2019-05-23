9bac8d2da5a1c0da101a19733b21b58df6aa4730
Salim Benouamer <salim@wity.fr>
2019-05-23 09:36:28 +0200

    First version

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+//About scss
+require('../scss/app.scss');
+
+
+// About js
+const $ = require('jquery');
+// create global $ and jQuery variables
+global.$ = global.jQuery = $;
+
+require('bootstrap');
